97dc5bc81d85fdc45052d20969399a6bcb0779df
Martti Tuisku <martti.tuisku@sherpas.se>
2022-06-21 10:31:45 +0200

    Add project files.

---
+++ AW_Fullstack_W3_D2_EF_Example/Models/Topic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AW_Fullstack_W3_D2_EF_Example.Models
+{
+    public partial class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? TimeToMaster { get; set; }
+        public int? TimeSpent { get; set; }
+        public string Source { get; set; }
+        public DateTime? StartLearningDate { get; set; }
+        public bool? InProgress { get; set; }
+    }
+}
---
+++ AW_Fullstack_W3_D2_EF_Example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AW_Fullstack_W3_D2_EF_Example.Models;
+using System.Linq;
+
+namespace AW_Fullstack_W3_D2_EF_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (LearningDiaryContext testiYhteys = new LearningDiaryContext())
+            {
+                var taulu = testiYhteys.Topics.Select(topikki => topikki);
+                Topic uusi = new Topic()
+                {
+                    Id = 1,
+                    Description = "Testi",
+                    Title = "Title",
+                    TimeToMaster = 3,
+                    TimeSpent = 2,
+                    StartLearningDate = DateTime.Now,
+                    InProgress = false
+                };
+                testiYhteys.Topics.Add(uusi);
+                testiYhteys.SaveChanges();
+
+                taulu = testiYhteys.Topics.Select(topikki => topikki);
+                foreach(var rivi in taulu)
+                {
+                    Console.WriteLine(rivi.Description);
+                }
+
+                //Console.WriteLine(taulu);
+            }
+        }
+    }
+}
